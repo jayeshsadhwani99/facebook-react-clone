887fa7d77ea138c31a3cd3792eec01644f6a7c41
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2021-01-30 22:27:26 +0530

    second commit

---
+++ fb-clone/src/MessageSender.js
@@ -0,0 +1,43 @@
+import { Avatar } from '@material-ui/core';
+import React from 'react';
+import './MessageSender.css';
+// import VideoCamIcon from "@material-ui/icons";
+// import PhotoLibraryIcon from "@material-ui/icons/PhotoLibrary";
+// import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+
+function MessageSender() {
+    const handleSubmit = e => {
+        e.preventDefault();
+    }
+    return (
+        <div className="messageSender">
+            <div className="messageSender__top">
+                <Avatar />
+                <form>
+                    <input className="messageSender__input" placeholder={"What's on your mind?"}/>
+                    <input placeholder="Image URL (Optional)" />
+                    <button onClick={handleSubmit} type="submit">Hidden Submit</button>
+                </form>
+            </div>
+            
+            <div className="messageSender__bottom">
+                <div className="messageSender__option">
+                    {/* <VideoCamIcon style={{ color: 'red' }} /> */}
+                    <h3>Live Video</h3>
+                </div>
+
+                <div className="messageSender__option">
+                    {/* <PhotoLibraryIcon style={{ color: 'green' }} /> */}
+                    <h3>Photo/Video</h3>
+                </div>
+
+                <div className="messageSender__option">
+                    {/* <InsertEmoticonIcon style={{ color: 'orange' }} /> */}
+                    <h3>Feeling/Activity</h3>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MessageSender
